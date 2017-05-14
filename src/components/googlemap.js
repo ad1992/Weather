a9a13f14e1ef91e6c76bb75b3f7095a04f583792
Aakansha Doshi <aakansha1216@gmail.com>
2017-05-14 19:25:01 +0530

    make google map a functional component

--- src/components/googlemap.js
+++ src/components/googlemap.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-
-class GoogleMap extends Component {
-  componentDidMount() {
-    new google.maps.Map(this.refs.map, {
-      zoom: 12,
-      center: {
-        lat: this.props.lat,
-        lng: this.props.lon
+import React from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+export default props => {
+  return (
+    <GoogleMapLoader
+      containerElement={<div style={{ height: '100%' }} />}
+      googleMapElement={
+        <GoogleMap
+          defaultZoom={12}
+          defaultCenter={{ lat: props.lat, lng: props.lon }}
+        />
       }
-    });
-  }
-  render() {
-    return <div ref="map" />;
-  }
-}
-
-export default GoogleMap;
+    />
+  );
+};
